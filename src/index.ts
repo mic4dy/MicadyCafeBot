ae53a30ba26cbfdbbb042d86262679f5d05db235
micady <micady.dev@gmail.com>
2024-08-18 17:06:48 +0900

    feat: add intents

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Client } from "discord.js";
+import { Client, GatewayIntentBits } from "discord.js";
 import dotenv from "dotenv";
 import { CommandHandler } from "./commands/command_handler";
 import { EventHandler } from "./events/event_handler";
@@ -6,7 +6,11 @@ import { EventHandler } from "./events/event_handler";
 dotenv.config();
 
 const client = new Client({
-  intents: [],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+  ],
   partials: [],
 });
 
